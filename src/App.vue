47c7874d8d4eadaa6cffc43fe63a1121d2dd7ff9
fxbd5684 <136948328+fxbd5684@users.noreply.github.com>
2024-08-11 20:17:42 +0800

    更改代码,尝试

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+    <h1>Welcome to web-of-XNORS</h1>
+  </div>
+  <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
