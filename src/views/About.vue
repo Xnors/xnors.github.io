fd0757186f0d2089bc943f2565ca6c89251949f9
fxbd5684 <136948328+fxbd5684@users.noreply.github.com>
2024-08-13 18:57:06 +0800

    修复滚动介绍BUG 介绍以后在About.vue文件里面开发

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,58 @@
 <template>
   <div>
-    <h1>About</h1>
-    <h1>{{ msg }}</h1>
+    <h1>关于我们:</h1>
+    <div class="intro-text">这是一条介绍我们的文本</div>
   </div>
 </template>
 
-<script setup>
-import { defineProps } from "vue";
-defineProps({
-  msg: String,
-});
-
+<script>
+/* 导出组件 */
+export default {
+  name: "About",
+  data() {
+    return {};
+  },
+};
 </script>
 
-<style></style>
+<style type="text/scss">
+@keyframes fideIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.intro {
+  font-family: "deyihei";
+
+  font-size: 200%;
+  text-align: left;
+  opacity: 1;
+
+  color: white;
+  animation: fideIn 1s ease-in-out;
+  .intro-text {
+    font-size: 80%;
+    text-align: center;
+    animation: fideIn 1s ease-in-out;
+  }
+}
+
+.home-container {
+  height: 100vh;
+  display: flex;
+  color: azure;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,6 @@
 <script>
+import About from "./About.vue";
+
 export default {
   name: "Home",
   data() {
@@ -6,12 +8,13 @@ export default {
       nav_show: false,
     };
   },
+
   methods: {
     handleScroll() {
       // 获取页面高度
       this.pageHeight = document.documentElement.scrollHeight;
       // 页面滑动的距离
-      let scrollTop = document.documentElement.scrollTop;
+      let scrollTop = document.documentElement.scrollTop + 60;
       // 当页面滑动的距离大于页面高度时元素显示，否则不显示
       if (scrollTop >= this.pageHeight - window.innerHeight) {
         this.nav_show = true;
@@ -19,6 +22,9 @@ export default {
         this.nav_show = false;
       }
     },
+  },
+  components: {
+    About,
   },
   mounted() {
     window.addEventListener("scroll", this.handleScroll);
@@ -32,26 +38,38 @@ export default {
 
 <template>
   <!-- SVG内容 -->
-  <img src="../assets/svgs/LT-logo-name.svg" class="左上logo" alt="" />
+  <img
+    src="../assets/svgs/LT-logo-name.svg"
+    class="左上logo"
+    alt=""
+    style="height: 80%; width: 80%"
+  />
   <img src="../assets/svgs/line.svg" class="分割线" alt="" />
   <div class="set-center">
-    <img class="logo" src="../assets/svgs/CENTER-logo-name-motto.svg" />
+    <img
+      class="logo"
+      src="../assets/svgs/CENTER-logo-name-motto.svg"
+      style="width: 100%; height: 100%"
+      alt=""
+    />
 
     <div class="aboutus">
       <p href="/about">Know More<br />↓</p>
     </div>
+    <!--     <a href="https://github.com/xnors/" class="github-link">
+      <img src="../assets/gh-icon-and-text.svg" alt="" />
+    </a> -->
   </div>
   <br />
 
   <div class="home-container">
     <div class="intro" v-if="nav_show">
-      <h1>关于我们:</h1>
-      <div class="intro-text">这是一条介绍我们的文本</div>
+      <About />
     </div>
   </div>
 </template>
 
-<style type="text/css" scoped>
+<style type="text/scss" scoped>
 @import url("../styles/home.scss");
-@import url("../styles/home.intro.scss");
+@import url("../styles/bootstrap.css");
 </style>
