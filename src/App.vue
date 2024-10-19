bc1df093c51ea150fb9fc9df165459d1ca8c64ec
fxbd5684 <136948328+fxbd5684@users.noreply.github.com>
2024-10-19 13:34:05 +0800

    全部代码清除

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
-<script setup lang="ts">
-import Home from './views/Home.vue';
+<script>
+import Home from './views/Home.vue'
+export default {
+  components: {
+    Home,
+  },
+}
 </script>
 
 <template>
-  <main>
-    <Home/>
-  </main>
+  <div class="home">
+    <Home />
+  </div>
+  <div class="home2">
+    <Home />
+  </div>
 </template>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+}
+
+.home {
+  height: 100vh;
+  width: 100vw;
+
+  background-color: #000;
+}
+
+.home2 {
+  height: 100vh;
+  width: 100vw;
+
+  background-color: #ffffff;
+}
+</style>
