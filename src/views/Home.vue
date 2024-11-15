f9eba3d0e4422cb5d8af79de2031e060b5ab17f7
fxbd5684 <136948328+fxbd5684@users.noreply.github.com>
2024-11-15 18:24:20 +0800

    修改主页颜色

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -204,7 +204,7 @@ a {
     align-items: center;
     height: 100vh;
 
-    background: linear-gradient(318deg, #1a1022, #111d22);
+    background: linear-gradient(318deg, #281a33, #232542, #233942);
     background-size: 400% 400%;
     animation: gradient 10s ease infinite;
 }
@@ -215,6 +215,6 @@ a {
     max-width: 50%;
     max-height: 50%;
 
-    mix-blend-mode: difference
+    // mix-blend-mode: difference
 }
 </style>
